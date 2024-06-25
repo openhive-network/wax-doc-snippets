e5e7213457edc947980d096432f2ff99b7f1aeed
mtyszczak <mateusz.tyszczak@gmail.com>
2024-06-25 16:51:31 +0200

    Modify the script to work in the stackblitz environment

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -1,49 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-SCRIPT_PATH=$(dirname $(realpath -s $0))
-PROJECT_PATH=$(realpath -s $SCRIPT_PATH/..)
+# We operate in the stackblitz environment, which is a bit different from the standard shell environment
+TEST_FILE=${2:-$1}
 
-TEST_FILE=$1
+echo $TEST_FILE
 
-if [[ -z "$TEST_FILE" || ! -f "$PROJECT_PATH/$TEST_FILE" ]]; then
-  echo "Usage: npm test <test-file>"
-  exit 1
-fi
+rm ./*.js
 
-DIST_DIR=dist/test
+tsc --outDir . --skipLibCheck -m node16 "$TEST_FILE"
 
-tsc --outDir "$PROJECT_PATH/$DIST_DIR" --skipLibCheck -m node16 "$TEST_FILE"
+cp scripts/runner.js runner.js
 
-TEST_FILENAME=$(basename -- "$TEST_FILE")
-TEST_FILENAME_NOEXT="${TEST_FILENAME%.*}"
+rm -r storage_root-node
 
-mv "$PROJECT_PATH/$DIST_DIR/$TEST_FILENAME_NOEXT.js" "$PROJECT_PATH/$DIST_DIR/testfile.js"
-
-cat << EOF > "$PROJECT_PATH/$DIST_DIR/runner.js"
-import beekeeperFactory from '@hiveio/beekeeper';
-
-const beekeeper = await beekeeperFactory();
-
-const session = await beekeeper.createSession("my.salt");
-
-const { wallet, password } = await session.createWallet('w0');
-
-const publicKey1 = await wallet.importKey('5JkFnXrLM2ap9t3AmAxBJvQHF7xSKtnTrCTginQCkhzU5S7ecPT');
-const publicKey2 = await wallet.importKey('5KGKYWMXReJewfj5M29APNMqGEu173DzvHv5TeJAg9SkjUeQV78');
-
-globalThis.snippetsBeekeeperData = { wallet, password, publicKey1, publicKey2 };
-
-import './testfile.js';
-EOF
-
-BEEKEEPER_DIR="$PROJECT_PATH/storage_root-node"
-
-if [[ -d "$BEEKEEPER_DIR" ]]; then
-  rm -r "$BEEKEEPER_DIR"
-fi
-
-pushd "$PROJECT_PATH"
-
-node "$PROJECT_PATH/$DIST_DIR/runner.js"
-
-popd
+node runner.js
---
+++ scripts/runner.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import beekeeperFactory from '@hiveio/beekeeper';
+
+const jsFiles = fs.readdirSync('.').filter(file => file.endsWith('.js') && !file.endsWith('/runner.js')).map(file => `./${file}`);
+
+const beekeeper = await beekeeperFactory();
+
+const session = await beekeeper.createSession('my.salt');
+
+const { wallet, password } = await session.createWallet('w0');
+
+const publicKey1 = await wallet.importKey(
+  '5JkFnXrLM2ap9t3AmAxBJvQHF7xSKtnTrCTginQCkhzU5S7ecPT'
+);
+const publicKey2 = await wallet.importKey(
+  '5KGKYWMXReJewfj5M29APNMqGEu173DzvHv5TeJAg9SkjUeQV78'
+);
+
+globalThis.snippetsBeekeeperData = { wallet, password, publicKey1, publicKey2 };
+
+// We assume that there is only one output file
+await import(jsFiles[0]);
