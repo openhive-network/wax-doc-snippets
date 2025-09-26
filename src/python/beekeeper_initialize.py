3c9d52cf691e69d2fe96abbc0aa85e8bbf07d62b
Michał Kudela <kudmich@syncad.com>
2025-09-26 10:41:39 +0200

    Enhance initialize beekeeper snippet ( import keys to wallet )

--- src/python/beekeeper_initialize.py
+++ src/python/beekeeper_initialize.py
@@ -7,7 +7,14 @@ with Beekeeper.factory() as bk:
     # Create a wallet with a given name and password
     wallet = session.create_wallet(name="my-wallet-name", password="my-wallet-password")
 
+    # Import a private key into the wallet
+    wallet.import_key(private_key="5KgfcV9bgEen3v9mxkoGw6Rhuf2giDRZTHZjzwisjkrpF4FUh3N")
+    # Check if the wallet contains the matching private key
+    assert wallet.has_matching_private_key(key="STM5gQPYm5bs9dRPHpqBy6dU32M8FcoKYFdF4YWEChUarc9FdYHzn")
+
     # Unlock the wallet so it can be used for signing and other operations
     wallet.unlock(password="my-wallet-password")
+    # Ensure the wallet is unlocked
+    assert wallet.is_unlocked()
 
     # At this point, the wallet is ready
